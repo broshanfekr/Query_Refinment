43bf0e11c75d2701c187b436b0aebbb7ae8a35ac
broshanfekr <bero.1370@yahoo.com>
2015-08-02 20:33:57 +0430

    extended

--- pgm-tmp.py
+++ pgm-tmp.py
@@ -11,6 +11,7 @@ from  time import time
 '''########################################Variables#######################################'''
 Train_Set = []
 Language_Model = []
+Dictionary = []
 ###########################################################################################
 '''#######################################Functions########################################'''
 def to_dict(lm_df):
@@ -26,101 +27,11 @@ def f(yi_1, yi, lm):
     else:
         return -100
 #------------------------------------------------------------------------------------------
-def Get_Probability(x, lm):
-    if x in lm.keys():
-        return lm[x]
+def Get_Probability(x, Dic):
+    if x in Dic.keys():
+        return Dic[x]
     else:
         return -50
-#------------------------------------------------------------------------------------------
-def h(yi, oi, x, i):# All inputs are string exept i. i is integer
-    yi = yi.lower()
-    x = x.lower()
-    if oi == "Nothing":
-        return 1
-    elif oi == "Deletion":   #Spelling Correction-> Deletion
-        myx = x.split(" ")
-        myx = myx[i]
-        for k in range(len(myx)):
-            begin = myx[:k]    # from beginning to n (n not included)
-            end = myx[k+1:]    # n+1 through end of string
-            tmp_string = begin + end
-            if yi == tmp_string:
-                return 1
-        return 0
-    elif oi == "Stemming":
-        myx = x.split(" ")
-        myx = myx[i]
-        list = ["ing", "ed", "s", "es"]
-        for k in list:
-            tmp_string = myx + k
-            if yi == tmp_string:
-                return 1
-        return 0
-    elif oi == "Insertion":
-        myx = x.split(" ")
-        myx = myx[i]
-        list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-                'u', 'v', 'w', 'x', 'y', 'z']
-        for k in range(len(myx)+1):
-            for char in list:
-                begin = myx[:k]    # from beginning to n (n not included)
-                end = myx[k:]    # n+1 through end of string
-                tmp_string = begin + char + end
-                if yi == tmp_string:
-                    return 1
-        return 0
-    elif oi == "Substitution":
-        myx = x.split(" ")
-        myx = myx[i]
-        list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-                'u', 'v', 'w', 'x', 'y', 'z']
-        for k in range(len(myx)):
-            for char in list:
-                begin = myx[:k]    # from beginning to n (n not included)
-                end = myx[k+1:]    # n+1 through end of string
-                tmp_string = begin + char + end
-                if yi == tmp_string:
-                    return 1
-        return 0
-    elif oi == "Transposition":
-        myx = x.split(" ")
-        myx = myx[i]
-        t = []
-        for char in myx:
-            t.append(char)
-        myx = t
-        for k1 in range(len(myx)):
-            for k2 in range(k1+1, len(myx)):
-                tmp = myx[k1]
-                myx[k1] = myx[k2]
-                myx[k2] = tmp
-                tmp_string = ""
-                for kk in range(len(myx)):
-                    tmp_string = tmp_string + myx[kk]
-                if yi == tmp_string:
-                    return 1
-        return 0
-    elif oi == "Splitting":
-        myx = x.split(" ")
-        myx = myx[i]
-        char = '-'
-        for k in range(1, len(myx)):
-            begin = myx[:k]    # from beginning to n (n not included)
-            end = myx[k:]    # n+1 through end of string
-            tmp_string = begin + char + end
-            if yi == tmp_string:
-                return 1
-        return 0
-    elif oi == "Merging":
-        myx = x.split(" ")
-        myxi = myx[i]
-        myxii = myx[i+1]
-        tmp_string = myxi + "-" + myxii
-        if yi == tmp_string:
-            return 1
-        return 0
-    elif oi == "Merged_Before":
-        print("do nothing")
 #------------------------------------------------------------------------------------------
 def Deletion(x):
     P_List = []
@@ -130,6 +41,7 @@ def Deletion(x):
         tmp_string = begin + end
         P_List.append(tmp_string)
     return P_List
+
 def Stemming(x):
     P_List = []
     list = ["ing", "ed", "s", "es"]
@@ -137,6 +49,7 @@ def Stemming(x):
         tmp_string = x + k
         P_List.append(tmp_string)
     return P_List
+
 def Insertion(x):
     P_List = []
     list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
@@ -148,6 +61,7 @@ def Insertion(x):
             tmp_string = begin + char + end
             P_List.append(tmp_string)
     return P_List
+
 def Substitution(x):
     P_List = []
     list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
@@ -159,6 +73,7 @@ def Substitution(x):
             tmp_string = begin + char + end
             P_List.append(tmp_string)
     return P_List
+
 def Transposition(x):
     P_List = []
     t = []
@@ -174,8 +89,8 @@ def Transposition(x):
             for kk in range(len(x)):
                 tmp_string = tmp_string + x[kk]
             P_List.append(tmp_string)
-
     return P_List
+
 def Splitting(x):
     P_List = []
     char = '#'
@@ -228,7 +143,7 @@ def Build_Y(X, index, zi, O):
 
     return [Z, o]
 #------------------------------------------------------------------------------------------
-def isword(x, lm):
+def isword(x, Dic):
     if abs(x.find('#') - x.find('-')) == 1:
         return 0
     dash_point = x.find('-')
@@ -240,34 +155,34 @@ def isword(x, lm):
             begin = first[:sharp_point]
             end = first[sharp_point+1:]
             tmp_str = begin + end
-            if Get_Probability(tmp_str, lm) < -49:
+            if Get_Probability(tmp_str, Dic) < -49:
                 return 0
         else:
-            if Get_Probability(first, lm) < -49:
+            if Get_Probability(first, Dic) < -49:
                 return 0
         sharp_point = secound.find('#')
         if sharp_point != -1:
             begin = secound[:sharp_point]
             end = secound[sharp_point+1:]
             tmp_str = begin + end
-            if Get_Probability(tmp_str, lm) < -49:
+            if Get_Probability(tmp_str, Dic) < -49:
                 return 0
         else:
-            if Get_Probability(secound, lm) < -49:
+            if Get_Probability(secound, Dic) < -49:
                 return 0
     sharp_point = x.find('#')
     if sharp_point != -1:
         begin = x[:sharp_point]
         end = x[sharp_point+1:]
         tmp_str = begin + end
-        if Get_Probability(tmp_str, lm) < -49:
+        if Get_Probability(tmp_str, Dic) < -49:
             return 0
     else:
-        if Get_Probability(x, lm) < -49:
+        if Get_Probability(x, Dic) < -49:
             return 0
     return 1
 #------------------------------------------------------------------------------------------
-def Get_Possible_Y(X, index, lm):
+def Get_Possible_Y(X, index, Dic):
     x = X.split(" ")
 
     zi = x[index]
@@ -295,7 +210,7 @@ def Get_Possible_Y(X, index, lm):
 #        for j in range(len(ZZ)):
 #            [ZZZ, ooo] = Build_Y(X, index, ZZ[j], "Split")
 #            for k in range(len(ZZZ)):
-#                if isword(ZZZ[k], lm) == 1 or (o[i] == "Nothing" and oo[j] == "Nothing" and ooo[k] == "Nothing"):
+#                if isword(ZZZ[k], Dic) == 1 or (o[i] == "Nothing" and oo[j] == "Nothing" and ooo[k] == "Nothing"):
 #                    Y.append([Z[i], ZZ[j], ZZZ[k]])
 #                    O.append([o[i], oo[j], ooo[k]])
 
@@ -330,7 +245,7 @@ def Get_Possible_Y(X, index, lm):
         for j in range(len(ZZ)):
             [ZZZ, ooo] = Build_Y(X, index, ZZ[j], "Spell")
             for k in range(len(ZZZ)):
-                if isword(ZZZ[k], lm) == 1 or (o[i] == "Nothing" and oo[j] == "Nothing" and ooo[k] == "Nothing"):
+                if isword(ZZZ[k], Dic) == 1 or (o[i] == "Nothing" and oo[j] == "Nothing" and ooo[k] == "Nothing"):
                     Y.append([Z[i], ZZ[j], ZZZ[k]])
                     O.append([o[i], oo[j], ooo[k]])
 
@@ -343,13 +258,13 @@ def Get_Possible_Y(X, index, lm):
 #        for j in range(len(ZZ)):
 #            [ZZZ, ooo] = Build_Y(X, index, ZZ[j], "Split")
 #            for k in range(len(ZZZ)):
-#                if isword(ZZZ[k], lm) == 1 or (o[i] == "Nothing" and oo[j] == "Nothing" and ooo[k] == "Nothing"):
+#                if isword(ZZZ[k], Dic) == 1 or (o[i] == "Nothing" and oo[j] == "Nothing" and ooo[k] == "Nothing"):
 #                    Y.append([Z[i], ZZ[j], ZZZ[k]])
 #                    O.append([o[i], oo[j], ooo[k]])
 
     return [Y, O]
 #------------------------------------------------------------------------------------------
-def Test_Model(X, lamda, lm):    #X is the input string, lamda is the parameter
+def Test_Model(X, lamda, lm, Dic):    #X is the input string, lamda is the parameter
     x = X.split(" ")
     Best_O = []
     Best_y = []
@@ -368,7 +283,7 @@ def Test_Model(X, lamda, lm):    #X is the input string, lamda is the parameter
             index += 1
             continue
 
-        [Y, O] = Get_Possible_Y(X, i, lm)
+        [Y, O] = Get_Possible_Y(X, i, Dic)
 
         for yi in range (len(Y)):
             zi = Y[yi][len(Y[yi])-1]
@@ -418,37 +333,13 @@ def F_Function(lamda):
     Train_Data = Train_Set
     lm = Language_Model
     sum = 0
-
     for i in range(1, len(Train_Data)):
-        X = Train_Data.ix[i, 0]
         Y = Train_Data.ix[i, 1]
-        O = Train_Data.ix[i, 2]
-        O = O.strip()
-        x = X.split(" ")
         y = Y.split(" ")
-        o = O.split(" ")
         yi_1 = "<s>"
-#        print(i)
-        if i == 18:
-            continue
-        if i == 60 or i == 61 or i == 64:
-            continue
-        if i == 100:
-            print("suse")
-        for j in range(len(x)):
-            xi = x[j]
+        for j in range(len(y)):
             yi = y[j]
-            oi = o[j]
-            if yi == '-':
-                tmp = yi_1.split("-")
-                yi_1 = tmp[0] + tmp[1]
-                continue
-            #print(lamda)
-            if oi == "Merging":
-                tmp = yi.split("-")
-                score = (lamda * f(yi_1, tmp[0] + tmp[1], lm) + lamda * h(yi, oi, X, j))
-            else:
-                score = (lamda * f(yi_1, yi, lm) + lamda * h(yi, oi, X, j))
+            score =  lamda * f(yi_1, yi, lm) + lamda * 1 #h(yi, oi, X, j))
             yi_1 = yi
             sum += score
     nnnn = norm(lamda)
@@ -457,56 +348,35 @@ def F_Function(lamda):
     print "landa is: ", lamda
     result= -1 * sum
     return result
-
 '''###########################################################################################'''
 if __name__ == "__main__":
-
 
     myfile = open("train.csv", "r")
     for line in myfile:
         line = line.strip("\n")
         currentline = line.split(",")
-        act = currentline[3]
-        act.strip()
-        act.split("-")
-        mystring = ""
-        for s in act:
-            if s == "n":
-                mystring = mystring + "Nothing "
-            elif s == "d":
-                mystring = mystring + "Deletion "
-            elif s == "i":
-                mystring = mystring + "Insertion "
-            elif s == "m":
-                mystring = mystring + "Merging "
-            elif s == "mb":
-                mystring = mystring + "Merged_Before "
+        Train_Set.append([currentline[0], currentline[1]])
 
-        Train_Set.append([currentline[1], currentline[2],mystring])
     train = pd.DataFrame(Train_Set)
     Train_Set = train
-
     myfile.close()
 
-    Language_Model = pd.DataFrame().from_csv("lm.csv")
+    Language_Model = pd.DataFrame().from_csv("bigram.csv")
     Language_Model.columns = ["digram", "log prob"]
     Language_Model = to_dict(Language_Model)
 
+    Dictionary = pd.DataFrame().from_csv("onegram.csv")
+    Dictionary.columns = ["digram", "log prob"]
+    Dictionary = to_dict(Dictionary)
+
     #Get_Possible_Y("ho w to mace catt", 1, Language_Model)
-    #landa = optimize.minimize(F_Function, [1], method='L-BFGS-B', options=dict({'maxiter':10}))
+    landa = optimize.minimize(F_Function, [1], method='L-BFGS-B', bounds = ((0, None),) ,options=dict({'maxiter':10}))
 
     start = time()
-    #O_landa = landa.x[0]
-    #O_landa = 1
-    #x = Test_Model("ho w to mace catt", O_landa, Language_Model)
-    #print(x)
-    #print("--------------finish-----------------")
-
-    x = "ho"
-    y = 0
-    for i in range(10000):
-        if Get_Probability(x , Language_Model) > -50:
-            y = 1
-    print(y)
-
+    O_landa = landa.x[0]
+    print "best landa is: " , O_landa
+    #O_landa = 0.000793534564046
+    x = Test_Model("ho w to mace catt", O_landa, Language_Model, Dictionary)
+    print(x)
+    print("--------------finish-----------------")
     print (time()-start)
